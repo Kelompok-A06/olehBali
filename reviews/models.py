b3828795709d74fe8090c36a644f9662d7414594
MHibriziF <hibrizifarghana@gmail.com>
2024-10-23 22:35:21 +0700

    add: review models

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+
+class TempProduct(models.Model):
+    pass
+
+class Reviews(models.Model):
+    RATINGS_CHOICE = (0,1,2,3,4,5)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(TempProduct, on_delete=models.CASCADE)
+    ratings = models.IntegerField(choices=RATINGS_CHOICE, default=0)
+    comments = models.TextChoices()
+
