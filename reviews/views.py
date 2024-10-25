129e7864fe0583ebcf38cc62e5a7c1d1e23a9fdb
MHibriziF <hibrizifarghana@gmail.com>
2024-10-25 20:02:24 +0700

    create routings

--- reviews/views.py
+++ reviews/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_review(request):
-    render(request, "product-review.html")
+    return render(request, "product-review.html")
