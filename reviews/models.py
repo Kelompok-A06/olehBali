b6f4299e72576c803484b4d0c78462426925fd38
MHibriziF <hibrizifarghana@gmail.com>
2024-10-25 16:13:36 +0700

    adjust reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -1,16 +1,19 @@
 from django.db import models
-from authentication.models import User
-
+from django.conf import settings
+from catalog.models import Product
 # Create your models here.
 
-
-class TempProduct(models.Model):
-    pass
-
 class Reviews(models.Model):
-    RATINGS_CHOICE = (0,1,2,3,4,5)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(TempProduct, on_delete=models.CASCADE)
+    RATINGS_CHOICE = [
+        (0, 0),
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+    ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     ratings = models.IntegerField(choices=RATINGS_CHOICE, default=0)
-    comments = models.TextChoices()
+    comments = models.TextField()
 
