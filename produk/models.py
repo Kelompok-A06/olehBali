3e79968bf80c073cd9c3eaf72d05fff69b26b0b3
MHibriziF <hibrizifarghana@gmail.com>
2024-10-25 13:35:00 +0700

    temporary product placement

--- produk/models.py
+++ produk/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    JENIS_KATEGORI = [
+        ('kerajinan_tangan', 'Kerajinan Tangan'),
+        ('makanan_minuman', 'Makanan/Minuman'),
+        ('pakaian', 'Pakaian'),
+        ('lain_lain', 'Lain-lain'),
+    ]
+    nama = models.CharField(max_length=255)
+    kategori = models.CharField(max_length=50, choices=JENIS_KATEGORI, default='lain-lain')
+    harga = models.IntegerField()
+    toko = models.CharField(max_length=255)
+    alamat = models.CharField(max_length=255)
+    deskripsi = models.TextField()
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    image_url = models.CharField(max_length=255, null=True, blank=True)
+
+
--- produk/views.py
+++ produk/views.py
@@ -1,3 +1,12 @@
+from .models import Product
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
+def get_products(request):
+    products = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", products), content_type="application/json")
+
+def get_products_by_id(request, id):
+    product = Product.objects.get(pk=id)
+    return HttpResponse(serializers.serialize("json", product), content_type="application/json")
