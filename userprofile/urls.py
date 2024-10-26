b7827a92ec99332b49dad5bd720b1a6d7ee9115d
kadeksavitri <kadeksavitri5@gmail.com>
2024-10-26 22:57:27 +0700

    done wishlist json and ajax

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('dashboard/<int:id>/', dashboard, name='dashboard'),
     path('update-profile/<int:id>/', update_profile, name='update_profile'),
     path('delete-account/', delete_account, name='delete_account'),
-    path('wishlist/', wishlist_dashboard, name='wishlist_dashboard'),
     path('json/<int:id>', show_user, name='show_user'),
 
 ]
--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from authentication.views import *
 from wishlist.views import *
-from . import views
 
 app_name = 'wishlist'
 urlpatterns =[
     path('', show_wishlist, name='show_wishlist'),
-    path('delete-wishlist/<int:id>', views.delete_wishlist, name="delete_wishlist"),
+    path('delete_wishlist/<int:product_id>/', delete_wishlist, name='delete_wishlist'),
+    path('json/show_wishlist', show_wishlist_json, name='show_wishlist_json'),
+    path('json/delete_wishlist/<int:product_id>/', delete_wishlist_json, name='delete_wishlist_json'),
 ]
