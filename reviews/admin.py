2228ca1630cb69af2ab8dc7266b45b51f830f921
kadeksavitri <kadeksavitri5@gmail.com>
2024-10-26 10:44:59 +0700

    modif admin.py dan modul wishlist

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Reviews
 
 # Register your models here.
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display =['user', 'product', 'ratings']
+admin.site.register(Reviews, ReviewsAdmin)
