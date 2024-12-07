ba141a0c0c9771bf0770dec7ac33b7cd1d1ec4f1
nraykhn <nuralyakhairina7@gmail.com>
2024-12-07 12:25:47 +0700

    Progres profile flutter

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -8,8 +8,12 @@ app_name = 'userprofile'
 
 urlpatterns = [
     path('dashboard/<int:id>/', dashboard, name='dashboard'),
+    path('api/profile/', get_profile, name='get_profile'),
+    path('dashboard_flutter/<int:id>/', dashboard_flutter, name='dashboard_flutter'),
     path('update-profile/<int:id>/', update_profile, name='update_profile'),
+    path('update_profile_flutter/<int:id>/', update_profile_flutter, name='update_profile_flutter'),
     path('delete-account/', delete_account, name='delete_account'),
+    path('delete_account_flutter/', delete_account_flutter, name='delete_account_flutter'),
     path('json/<int:id>', show_user, name='show_user'),
 ]
 
