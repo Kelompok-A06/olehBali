67528dcd9638c013d63b41c997a3697abe3e445e
nraykhn <nuralyakhairina7@gmail.com>
2024-12-09 13:58:21 +0700

    Tambah update dan delete account

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -9,11 +9,12 @@ app_name = 'userprofile'
 urlpatterns = [
     path('dashboard/<int:id>/', dashboard, name='dashboard'),
     path('api/profile/', get_profile, name='get_profile'),
+    path('api/get-role/', get_user_role, name='get_user_role'),
     path('dashboard_flutter/<int:id>/', dashboard_flutter, name='dashboard_flutter'),
     path('update-profile/<int:id>/', update_profile, name='update_profile'),
-    path('update_profile_flutter/<int:id>/', update_profile_flutter, name='update_profile_flutter'),
+    path('update_profile_flutter/', update_profile_flutter, name='update_profile_flutter'),
     path('delete-account/', delete_account, name='delete_account'),
-    path('delete_account_flutter/', delete_account_flutter, name='delete_account_flutter'),
+    path('delete-account-flutter/', delete_account_flutter, name='delete_account_flutter'),
     path('json/<int:id>', show_user, name='show_user'),
 ]
 
